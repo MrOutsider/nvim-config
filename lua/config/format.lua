4b3682e3dbb6444dff6912258a9f5cc1d13d24da
MrOutsider <16367984+MrOutsider@users.noreply.github.com>
2025-02-16 13:02:49 -0800

    Added line numbers

--- lua/config/format.lua
+++ lua/config/format.lua
@@ -2,3 +2,6 @@ vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+
+vim.wo.number = true -- Line numbers on the left
+vim.wo.relativenumber = true -- Relative line numbers on the left
