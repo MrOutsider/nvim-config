7ec5b02a88b5f0f6304db4b7038ab0d5120eed2f
MrOutsider <16367984+MrOutsider@users.noreply.github.com>
2025-02-16 02:08:53 -0800

    Added LSP clangd c lua_ls lua

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup()
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+        require("lspconfig").clangd.setup {}
+        require("lspconfig").lua_ls.setup {}
+        require("lspconfig").harper_ls.setup {}
+        end,
+    },
+}
