7f278b746241d41a5935ad347b987eb8a2907c13
MrOutsider <16367984+MrOutsider@users.noreply.github.com>
2025-02-16 12:57:53 -0800

    Commenting

--- lua/plugins/fuzzy-finder-telescope.lua
+++ lua/plugins/fuzzy-finder-telescope.lua
@@ -8,11 +8,11 @@ return {
       "BurntSushi/ripgrep",
     },
     config = function()
-      vim.keymap.set("n", "<Leader>fe", require("telescope.builtin").buffers)
-      vim.keymap.set("n", "<Leader>ff", require("telescope.builtin").find_files)
-      vim.keymap.set("n", "<Leader>fg", require("telescope.builtin").live_grep)
-      vim.keymap.set("n", "<Leader>fv", require("telescope.builtin").grep_string)
-      vim.keymap.set("n", "<Leader>fb", require("telescope.builtin").current_buffer_fuzzy_find)
+      vim.keymap.set("n", "<Leader>fe", require("telescope.builtin").buffers) -- View open buffers
+      vim.keymap.set("n", "<Leader>ff", require("telescope.builtin").find_files) -- Find file
+      vim.keymap.set("n", "<Leader>fg", require("telescope.builtin").live_grep) -- Find in files
+      vim.keymap.set("n", "<Leader>fv", require("telescope.builtin").grep_string) -- Find hovered word in files
+      vim.keymap.set("n", "<Leader>fb", require("telescope.builtin").current_buffer_fuzzy_find) -- Find in current buffer
     end,
   },
 }
