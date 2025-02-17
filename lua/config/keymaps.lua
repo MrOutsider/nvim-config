3529d42ce79ba1dddd981e705c34ad2d4fae0793
MrOutsider <16367984+MrOutsider@users.noreply.github.com>
2025-02-16 22:13:45 -0800

    New tab keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,6 +18,13 @@ vim.g.maplocalleader = " "
 --  command_mode = "c"
 
 -- Normal Mode --
+-- Tab
+keymap("n", "<leader><tab><tab>", ":tabnew<CR>", opts)
+-- Tab: Movement
+keymap("n", "<leader><tab>q", ":tabclose<CR>", opts)
+keymap("n", "<leader><tab>h", ":tabprevious<CR>", opts)
+keymap("n", "<leader><tab>l", ":tabnext<CR>", opts)
+
 -- Window
 keymap("n", "<leader>w", "<C-w>", opts)
 -- Window: Movment
@@ -25,15 +32,13 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
-
--- Window Dir Explorer
---keymap("n", "<Leader>e", ":Lex<CR> :vertical resize 30<CR>", opts)
-
 -- Window: Resize
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+-- Window Dir Explorer
+--keymap("n", "<Leader>e", ":Lex<CR> :vertical resize 30<CR>", opts)
 
 -- Buffer
 -- Buffer: Movement
@@ -41,15 +46,16 @@ keymap("n", "<S-h>", ":bnext<CR>", opts)
 keymap("n", "<S-l>", ":bprevious<CR>", opts)
 
 -- Format
-keymap("n", "<Leader>f", ":=vim.lsp.buf.format()<CR>", opts)
+keymap("n", "<leader>f", ":=vim.lsp.buf.format()<CR>", opts)
 
 -- File Explorer
-keymap("n", "<Leader>e", ":Ex<CR>", opts)
+keymap("n", "<leader>e", ":Ex<CR>", opts)
 
 -- Insert Mode --
-keymap("i", "jk", "<Esc>", opts)
+-- Escape Insert Mode
+keymap("i", "jk", "<esc>", opts)
 
 -- Visual Mode --
 -- Delete Things
-keymap("v", "<Leader>d", '"_d', opts) -- Send delete to null register
+keymap("v", "<leader>d", '"_d', opts) -- Send delete to null register
 keymap("v", "x", '"_x', opts)         -- Send single char delete to null register
