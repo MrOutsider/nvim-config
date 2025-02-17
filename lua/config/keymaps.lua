ff3207965e1d6c749550f1fff8896c6b5576c5a3
MrOutsider <16367984+MrOutsider@users.noreply.github.com>
2025-02-16 23:41:45 -0800

    Added Goto code def

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,8 @@
 -- Default Options
 local opts = { noremap = true, silent = true }
 
--- Shorten function name
-local keymap = vim.api.nvim_set_keymap
-
 -- Set Space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+vim.keymap.set("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -19,46 +16,51 @@ vim.g.maplocalleader = " "
 
 -- Normal Mode --
 -- Tab
-keymap("n", "<leader><tab><tab>", ":tabnew<CR>", opts)
-keymap("n", "<leader><tab>q", ":tabclose<CR>", opts)
+vim.keymap.set("n", "<leader><tab><tab>", ":tabnew<CR>", opts)
+vim.keymap.set("n", "<leader><tab>q", ":tabclose<CR>", opts)
 -- Tab: Movement
-keymap("n", "<leader><tab>h", ":tabprevious<CR>", opts)
-keymap("n", "<leader><tab>l", ":tabnext<CR>", opts)
+vim.keymap.set("n", "<leader><tab>h", ":tabprevious<CR>", opts)
+vim.keymap.set("n", "<leader><tab>l", ":tabnext<CR>", opts)
 
 -- Window
-keymap("n", "<leader>w", "<C-w>", opts)
--- Window: Movment
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+vim.keymap.set("n", "<leader>w", "<C-w>", opts)
+-- Window: Movement
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
 -- Window: Resize
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", opts)
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Window Dir Explorer
---keymap("n", "<Leader>e", ":Lex<CR> :vertical resize 30<CR>", opts)
+--vim.keymap.set("n", "<Leader>e", ":Lex<CR> :vertical resize 30<CR>", opts)
 
 -- Buffer
 -- Buffer: Movement
-keymap("n", "<S-h>", ":bnext<CR>", opts)
-keymap("n", "<S-l>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<S-h>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-l>", ":bprevious<CR>", opts)
 
 -- Format
-keymap("n", "<leader>f", ":=vim.lsp.buf.format()<CR>", opts)
+vim.keymap.set("n", "<leader>f", ":=vim.lsp.buf.format()<CR>", opts)
 
 -- File Explorer
-keymap("n", "<leader>e", ":Ex<CR>", opts)
+vim.keymap.set("n", "<leader>e", ":Ex<CR>", opts)
+
+-- LSP Definitions
+vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
+vim.keymap.set("n", "<leader>gD", function() vim.lsp.buf.declaration() end, opts)
+vim.keymap.set("n", "<leader>gt", function() vim.lsp.buf.type_definition() end, opts)
 
 -- Insert Mode --
 -- Escape Insert Mode
-keymap("i", "jk", "<esc>", opts)
+vim.keymap.set("i", "jk", "<esc>", opts)
 
 -- Visual Mode --
 -- Delete Things
-keymap("v", "<leader>d", '"_d', opts) -- Send delete to null register
-keymap("v", "x", '"_x', opts)         -- Send single char delete to null register
+vim.keymap.set("v", "<leader>d", '"_d', opts) -- Send delete to null register
+vim.keymap.set("v", "x", '"_x', opts)         -- Send single char delete to null register
 
 -- Terminal Mode --
-keymap("t", "<esc><esc>", "<C-\\><C-n>", opts)
+vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>", opts)
